1287f6d7027d44bde6f4b195b9ee7380d1adbb77
nickmwangemi <nickmwangemi@gmail.com>
2021-01-07 00:28:42 +0300

    Project Setup and deploy to Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+]
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Mini Messageboard', messages: messages })
+})
 
-module.exports = router;
+router.get('/new', (req, res) => {
+	res.render('form')
+})
+
+router.post('/new', (req, res) => {
+	console.log(req.body)
+	let user = req.body.messageUser
+	let text = req.body.messageText
+
+	messages.push({
+		text,
+		user,
+		added: new Date(),
+	})
+
+	console.log(messages)
+	res.redirect('/')
+})
+module.exports = router
